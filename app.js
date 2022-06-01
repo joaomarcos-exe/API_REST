c52134444d768a49fe4b8f10d265c3dfa431bf31
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-05-31 22:57:31 -0300

    IntroductionAbouSequelize

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-export default ()=>{
-    console.log('ola')
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import homeRouter from './src/routes/homeRoute';
+
+class App {
+    constructor(){
+        this.app = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: true}))
+        this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use('/', homeRouter)
+    }
 }
+
+export default new App().app
--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-import falaOi from './app';
-falaOi()
+import app from "./app";
+
+const port = 3001
+app.listen(port, ()=>{
+    console.log();
+    console.log(`Escutando na porta ${port}`);
+})
