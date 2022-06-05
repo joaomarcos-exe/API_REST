7bc17ded06105659a5080cbf1710989412dceb01
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-06-05 17:47:09 -0300

    UploadFiles

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import express from 'express';
 import homeRouter from './src/routes/homeRoute';
 import alunoRouter from './src/routes/alunoRoute';
 import userRouter from './src/routes/userRoute';
-import tokenRouter from './src/routes/tokenRoute'
+import tokenRouter from './src/routes/tokenRoute';
+import fotoRouter from './src/routes/fotoRoute'
 
 class App {
     constructor(){
@@ -24,6 +25,7 @@ class App {
         this.app.use('/users/', userRouter)
         this.app.use('/alunos/', alunoRouter)
         this.app.use('/token/', tokenRouter)
+        this.app.use('/foto/', fotoRouter)
     }
 }
 
---
+++ src/routes/fotoRoute.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import FotoController from "../controllers/FotoController";
+
+
+
+
+const router = new Router();
+
+router.post('/', FotoController.store)
+
+export default router;
